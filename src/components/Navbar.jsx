c6e63765b8fc8538b81572eabc66efddf0ac19ad
WimukthiPWijerathne <wijerathne_gpwp_e23@engug.ruh.ac.lk>
2025-01-26 22:02:37 +0530

    add href

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,13 @@ import { FaLinkedin, FaInstagram, FaFacebook } from 'react-icons/fa';
 export default function Navbar() {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
-      
       <div className='flex items-center gap-4'>
-        <FaLinkedin size={30} className='hover:text-blue-700' />
-        <FaInstagram size={30} className='hover:text-pink-500' />
+        <a href="https://www.linkedin.com/in/wimukthi-pathum-ba32b5233?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={30} className='hover:text-blue-700' />
+        </a>
+        <a href="https://www.instagram.com/wimukthi_13/?hl=en" target="_blank" rel="noopener noreferrer">
+          <FaInstagram size={30} className='hover:text-pink-500' />
+        </a>
         <FaFacebook size={30} className='hover:text-blue-600' />
       </div>
       <div className='m-8 flex items-center justify-center gap-4'>
@@ -18,7 +21,6 @@ export default function Navbar() {
           <li className='hover:underline'>Contact Me</li>
         </ul>
       </div>
-      
     </nav>
   );
 }
