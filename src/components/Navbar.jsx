050952a9c960073bbaf0b82a846f76c05d77bd21
WimukthiPWijerathne <wijerathne_gpwp_e23@engug.ruh.ac.lk>
2025-01-26 21:58:31 +0530

    add icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import logo from '../assets/kevinRushLogo.png';
-
+import { FaLinkedin, FaInstagram, FaFacebook } from 'react-icons/fa';
 
 export default function Navbar() {
   return (
     <nav className='mb-20 flex items-center justify-between py-6'>
-      <div className='flex flex-shrink-0 items-center'>
-        {/* <img className='mx-2 w-10' src={logo} alt="logo"  /> */}
+      
+      <div className='flex items-center gap-4'>
+        <FaLinkedin size={30} className='hover:text-blue-700' />
+        <FaInstagram size={30} className='hover:text-pink-500' />
+        <FaFacebook size={30} className='hover:text-blue-600' />
       </div>
-      <div className='m-8 flex items-center justify-center gap-4' >
-        <ul className='flex flex-wrap gap-5 hover:cursor-pointer bg-gradient-to-r from-pink-300 via-slate-500
-            to-purple-500 bg-clip-text text-xl tracking-tight text-transparent '>
+      <div className='m-8 flex items-center justify-center gap-4'>
+        <ul className='flex flex-wrap gap-5 hover:cursor-pointer bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-xl tracking-tight text-transparent'>
           <li className='hover:underline'>Home</li>
           <li className='hover:underline'>About Me</li>
           <li className='hover:underline'>Contact Me</li>
         </ul>
       </div>
+      
     </nav>
   );
 }
